603fec23d02fb5c75d5c81c9a65456dee9824d6d
YahArt <hutteryannick@gmail.com>
2021-11-06 13:26:57 +0100

    Updated welcome message again

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App with updated Content."/>
+    <HelloWorld msg="Welcome to Your Vue.js App with updated Content and more."/>
   </div>
 </template>
 
