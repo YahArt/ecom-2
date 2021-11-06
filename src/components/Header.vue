f1018ab851938033786aad629fbd98bd881c580d
YahArt <hutteryannick@gmail.com>
2021-11-06 14:51:40 +0100

    Integrate Google Fonts and UIKit

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 
+  <div class="header">
+    <img id="tailoredFitsLogo" alt="Tailored Fits Logo" src="../assets/images/tailored-fits-logo.png">
+    <a href=""></a>
+  </div>
+  -->
+  <nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="">Test</a></li>
+            <li class="uk-parent"><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </div>
+</nav>
+
+  
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {}
+}
+</script>
+
+<style scoped>
+.header {
+    height: 80px;
+    padding: 5px;
+    color: white;
+    display: flex;
+    flex-direction: row;
+}
+</style>
